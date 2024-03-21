a5f97701c00bfd189a53ac88ba85baa9593a81a2
Farid-SolaradAi <farid.ahmed@solarad.ai>
2024-03-21 13:12:06 +0530

    video

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -1,21 +1,8 @@
 "use client"
-import { getServerSession } from 'next-auth'
-import LoginForm from './form'
-import { redirect } from 'next/navigation'
-import { Button } from "@/components/ui/button"
+
 import * as Dialog from '@radix-ui/react-dialog';
 import { Cross2Icon } from '@radix-ui/react-icons';
-//import { Dialog,DialogContent,DialogDescription,DialogFooter,DialogHeader,DialogTitle,DialogTrigger } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+
 
 export default async function Video() {
     var items = [
@@ -74,7 +61,7 @@ export default async function Video() {
                       </div>
                     </a>
                 </Dialog.Trigger>
-                <Dialog.Portal style={{ width: '75vw', important: true }}>
+                <Dialog.Portal>
                     <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
                     <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
                         <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">

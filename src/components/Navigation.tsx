551720199d45c3a0592bd7bda62c6775360cb7f1
Farid-SolaradAi <farid.ahmed@solarad.ai>
2024-04-01 03:42:58 +0530

    add styles to logout button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -51,7 +51,7 @@ const Navigation = ({ children, ...props }: any) => {
                                         </div>
                                         <ul className="py-1" role="none">
                                             <li>
-                                                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem"><Logout /></a>
+                                                <Logout />
                                             </li>
                                         </ul>
                                     </div>
--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -6,7 +6,10 @@ export default async function Logout() {
     }
 
     return (
-        <span onClick={handleLogout}>
+        <span
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+            onClick={handleLogout}
+        >
             Sign Out
         </span>
     )

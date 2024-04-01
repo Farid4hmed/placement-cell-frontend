a27abaf97a6316eb52c12b978dfbb423c65307aa
Farid-SolaradAi <farid.ahmed@solarad.ai>
2024-04-01 13:46:07 +0530

    add reset-password route

---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,13 @@
+import ResetForm from './form'
+
+export default async function ResetPassword() {
+
+    return (
+        <main className="flex justify-center items-center h-screen">
+
+            <ResetForm />
+
+        </main>
+    )
+
+}
